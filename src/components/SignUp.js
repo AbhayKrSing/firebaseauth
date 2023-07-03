0f4914c27b1a4524d3c39fe699c00d1ea6e41be2
Abhay <kabhay849@gmail.com>
2023-07-03 19:37:27 +0530

    signup page Done

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -18,15 +18,15 @@ const SignUp = ({ children }) => {
                     <FormControl>
                         <Box m={5}>
                             <FormLabel>Email</FormLabel>
-                            <Input type='email' id='email' borderColor={'gray.400'} minLength={8} name='email' ref={emailref} onChange={handlechange} />
+                            <Input type='email' id='email' borderColor={'gray.400'} minLength={8} name='email' ref={emailref} onChange={handlechange} bgColor={'white'} />
                         </Box>
                         <Box m={5}>
                             <FormLabel>Password</FormLabel>
-                            <Input type='password' id='password' borderColor={'gray.400'} minLength={5} name='password' ref={passwordref} onChange={handlechange} />
+                            <Input type='password' id='password' borderColor={'gray.400'} minLength={5} name='password' ref={passwordref} onChange={handlechange} bgColor={'white'} />
                         </Box>
                         <Box m={5}>
                             <FormLabel>Confirm-Password</FormLabel>
-                            <Input type='password' id='cpassword' borderColor={'gray.400'} minLength={5} name='cpassword' ref={cpasswordref} onChange={handlechange} />
+                            <Input type='password' id='cpassword' borderColor={'gray.400'} minLength={5} name='cpassword' ref={cpasswordref} onChange={handlechange} bgColor={'white'} />
                         </Box>
                         <Box textAlign={'center'} m={8}>
                             <Button type='submit' colorScheme='green'>SignUp</Button>
